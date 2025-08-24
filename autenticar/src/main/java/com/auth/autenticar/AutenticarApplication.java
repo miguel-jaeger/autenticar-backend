497119a4f1484cf850641fd50167a5722a8b5e37
miguel.jaeger <miguel.jaeger@gmail.com>
2025-08-24 17:45:42 -0500

    add project

---
+++ autenticar/src/main/java/com/auth/autenticar/AutenticarApplication.java
@@ -0,0 +1,13 @@
+package com.auth.autenticar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticarApplication.class, args);
+	}
+
+}

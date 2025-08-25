b46804bace624b11d35d54fe29fa3a537a47e58c
henry chacon <hechacu@gmail.com>
2025-08-24 20:32:23 -0500

    login auntenticar

--- autenticar/src/main/java/com/auth/autenticar/service/ServicioUsuario.java
+++ autenticar/src/main/java/com/auth/autenticar/service/ServicioUsuario.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 import com.auth.autenticar.model.ModeloUsuario;
 import com.auth.autenticar.repository.IRepositorioUsuario;
 
-
+import java.util.Optional;
+ 
+import org.mindrot.jbcrypt.BCrypt;
 
 @Service
 public class ServicioUsuario {
@@ -32,5 +34,20 @@ public class ServicioUsuario {
     public void eliminarUsuario(ModeloUsuario usuario) {
          repositorioUsuario.deleteById(usuario.getIdPersona());
     }
+
+    public ModeloUsuario obtenerUsuarioPorId(Long id) {
+		return repositorioUsuario.findById(id).orElse(null);
+	}
+	
+	public boolean autenticarUsuario(String nombreUsuario, String password) {
+        ModeloUsuario usuarioOpt = repositorioUsuario.findByCorreo(nombreUsuario);
+        //password -> clave del formulario
+        // usuarioOpt.getContrasena() -> clave de base de datos
+        if (usuarioOpt!=null) {
+            return BCrypt.checkpw(password, usuarioOpt.getContrasena());
+        }
+        return false;
+     }
+
 }
 
